0a25ea03cfeaadec2b9c2f642ec66813b8af38f9
Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>
2015-12-01 13:58:51 -0500

    Fixed bridges

--- scratch.py
+++ scratch.py
@@ -1,10 +1,13 @@
 from mininet.node import Node
 from mininet.link import Link
 from mininet.log import setLogLevel, info
+from mininet.util import quietRun
+
+from time import sleep
 
 def default_network(cname='controller', cargs='-v ptcp:' ):
     """Create the surfnet test topology from scratch"""
-    CONTROLLER_IP = '192.168.1.30'
+    CONTROLLER_IP = '192.168.1.135'
     info('*** Creating Nodes\n')
     controller = Node('opendaylight', inNamespace=False)
     pe1 = Node('PE1', inNamespace=False)
@@ -32,22 +35,38 @@ def default_network(cname='controller', cargs='-v ptcp:' ):
     info("*** Starting network using Open vSwitch\n")
     controller.cmd(cname + ' ' + cargs + '&')
     pe1.cmd('ovs-vsctl del-br dp0')
-    pe1.cmd('ovs-vsctl add-br dp0')
+    pe1.cmd('ovs-vsctl add-br br0')
     pe2.cmd('ovs-vsctl del-br dp0')
-    pe2.cmd('ovs-vsctl add-br dp0')
+    pe2.cmd('ovs-vsctl add-br br1')
     pe3.cmd('ovs-vsctl del-br dp0')
-    pe3.cmd('ovs-vsctl add-br dp0')
+    pe3.cmd('ovs-vsctl add-br br2')
     p1.cmd('ovs-vsctl del-br dp0')
-    p1.cmd('ovs-vsctl add-br dp0')
+    p1.cmd('ovs-vsctl add-br br3')
     p2.cmd('ovs-vsctl del-br dp0')
-    p2.cmd('ovs-vsctl add-br dp0')
+    p2.cmd('ovs-vsctl add-br br4')
+    for intf in pe1.intfs.values():
+        print pe1.cmd('ovs-vsctl add-port br0 %s' % intf)
+    for intf in pe2.intfs.values():
+        print pe2.cmd('ovs-vsctl add-port br1 %s' % intf)
+    for intf in pe3.intfs.values():
+        print pe3.cmd('ovs-vsctl add-port br2 %s' % intf)
+    for intf in p1.intfs.values():
+        print p1.cmd('ovs-vsctl add-port br3 %s' % intf)
+    for intf in p2.intfs.values():
+        print p2.cmd('ovs-vsctl add-port br4 %s' % intf)
 
-    info('*** Setting controller on all the switches')
-    pe1.cmd('ovs-vsctl set-controller dp0 tcp:'+CONTROLLER_IP+':6633')
-    pe2.cmd('ovs-vsctl set-controller dp0 tcp:'+CONTROLLER_IP+':6633')
-    pe3.cmd('ovs-vsctl set-controller dp0 tcp:'+CONTROLLER_IP+':6633')
-    p1.cmd('ovs-vsctl set-controller dp0 tcp:'+CONTROLLER_IP+':6633')
-    p2.cmd('ovs-vsctl set-controller dp0 tcp:'+CONTROLLER_IP+':6633')
+    info('*** Setting controller on all the switches\n')
+    pe1.cmd('ovs-vsctl set-controller br0 tcp:'+CONTROLLER_IP+':6633')
+    pe2.cmd('ovs-vsctl set-controller br1 tcp:'+CONTROLLER_IP+':6633')
+    pe3.cmd('ovs-vsctl set-controller br2 tcp:'+CONTROLLER_IP+':6633')
+    p1.cmd('ovs-vsctl set-controller br3 tcp:'+CONTROLLER_IP+':6633')
+    p2.cmd('ovs-vsctl set-controller br4 tcp:'+CONTROLLER_IP+':6633')
+
+    info( '*** Waiting for switch to connect to controller' )
+    while 'is_connected' not in quietRun( 'ovs-vsctl show' ):
+        sleep( 1 )
+        info( '.' )
+    info( '\n' )
 
 if __name__ == '__main__':
     setLogLevel('info')
