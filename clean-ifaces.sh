a253a3afaffa0ac7d5b4420ae85fe4e9b45618d9
Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>
2015-12-02 12:31:00 -0500

    Added clean all bridges

--- clean-ifaces.sh
+++ clean-ifaces.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Deletes all previously created bridges that were created with scratch.py
+sudo ovs-vsctl der-br br0
+sudo ovs-vsctl der-br br1
+sudo ovs-vsctl der-br br2
+sudo ovs-vsctl der-br br3
+sudo ovs-vsctl der-br br4
+
 # Deletes any interfaces previously created
 sudo ip link del P2-eth2
 sudo ip link del PE3-eth0
